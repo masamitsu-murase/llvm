6da1b2ee5c4918182746d02ea341beb1747d3417
Masamitsu MURASE <masamitsu.murase@gmail.com>
2018-03-10 01:35:48 +0900

    Check.

--- my_build.bat
+++ my_build.bat
@@ -11,6 +11,7 @@ cd ..
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 if ERRORLEVEL 1 exit /b 1
 
+
 cd build
 
 cmake -G "Visual Studio 14 2015" .. -DCMAKE_INSTALL_PREFIX=..\install -DCMAKE_DEBUG_POSTFIX=d
